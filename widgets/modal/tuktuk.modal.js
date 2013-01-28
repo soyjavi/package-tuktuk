a668c24b9cb493935f77ac5bbf6eb61ccf4dbdb5
soyjavi <javi.jimenez.villar@gmail.com>
2013-01-28 20:26:31 +0100

    Initial Commit

---
+++ widgets/modal/tuktuk.modal.js
@@ -0,0 +1,58 @@
+var __slice = [].slice;
+
+if (!window.TukTuk) {
+  window.TukTuk = {
+    dom: function() {
+      var args;
+      args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+      if (typeof $$ !== "undefined" && $$ !== null) {
+        return $$.apply(null, args);
+      } else {
+        return $.apply(null, args);
+      }
+    }
+  };
+}
+
+window.TukTuk.Modal = (function(tk, undefined_) {
+  var hide, lock, modal, show;
+  lock = void 0;
+  modal = void 0;
+  /*
+        @todo: Describe method
+  */
+
+  show = function(modal_id) {
+    modal = tk.dom("[data-tuktuk=modal]#" + modal_id).first();
+    modal.addClass("active");
+    lock.addClass("active");
+    return this;
+  };
+  /*
+        @todo: Describe method
+  */
+
+  hide = function() {
+    lock.removeClass("active");
+    modal.removeClass("active");
+    return this;
+  };
+  return {
+    _Instance: (function() {
+      tk.dom("[data-tuktuk=modal].side").each(function(index, element) {
+        modal = tk.dom(element);
+        return modal.html("<div>" + modal.html() + "</div>");
+      });
+      tk.dom("[data-tuktuk=modal] [data-modal=close]").on("click", function() {
+        return TukTuk.Modal.hide();
+      });
+      tk.dom("[data-tuktuk-modal]").on("click", function() {
+        return TukTuk.Modal.show(tk.dom(this).attr('data-tuktuk-modal'));
+      });
+      tk.dom(document.body).append("<div data-tuktuk=\"lock\"></div>");
+      return lock = tk.dom("[data-tuktuk=lock]").first();
+    })(),
+    show: show,
+    hide: hide
+  };
+})(TukTuk);
